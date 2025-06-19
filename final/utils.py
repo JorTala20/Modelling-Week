e73ebb7a0ae0cfcdc8e77738cbb75e7041c014bd
Francesco Luppi Pittigliani <325040@studenti.unimore.it>
2025-06-19 13:01:32 +0200

    Create utils.py

---
+++ final/utils.py
@@ -0,0 +1,9 @@
+from zero_shot import  get_documents_by_class
+from prompt import generate_prompt
+from rag import generate_text
+from transformers import pipeline
+import time
+import requests
+
+HF_TOKEN = "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"  # Hugging Face token
+MODEL_ID = "mistralai/Mixtral-8x7B-Instruct-v0.1" # zero-shot model
